b3440098c83df6fcef7434820f7fe961da73d3ef
Alaa Rihan <alaa.addeen@gmail.com>
2019-06-18 07:48:18 +0300

    Fix a bug in updateCredit action

--- src/actions/updateCredit.js
+++ src/actions/updateCredit.js
@@ -9,9 +9,9 @@ async function updateCredit(page) {
     const account = await getCurrentAccount()
     if(!page) return false
     const currentPageUrl = page.url();
-    if (currentPageUrl !== 'https://www.like4like.org/user/earn-youtube-video.php') {
-        await page.goto('https://www.like4like.org/user/earn-youtube-video.php')
-    }
+    // if (currentPageUrl !== 'https://www.like4like.org/user/earn-youtube-video.php') {
+    //     await page.goto('https://www.like4like.org/user/earn-youtube-video.php')
+    // }
     if (page.url() === 'https://www.like4like.org/user/bonus-page.php') {
         await clickPuzzleMap(page)
         await page.waitFor(2000)
@@ -19,11 +19,12 @@ async function updateCredit(page) {
     }
     const currentCredit = await page.evaluate(
         () => document.querySelector('#earned-credits').innerText
-    )
+    ).catch(() => {
+        log('Couldn\'t get current credit', 'ERROR')
+    })
     if(currentCredit){
         updateAccountCredit(account.id, parseInt(currentCredit))
     }
-    await page.goto(currentPageUrl)
 }
 
 module.exports = updateCredit
