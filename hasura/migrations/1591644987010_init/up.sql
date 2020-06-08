3e8cdfd5c2600defe3546ade52438b7050ed9b9f
Alaa Rihan <alaa.addeen@gmail.com>
2020-06-08 22:38:40 +0300

    Update hasura migrations to v2

--- hasura/migrations/1591644987010_init/up.sql
+++ hasura/migrations/1591644987010_init/up.sql
@@ -3,13 +3,14 @@ CREATE TABLE public.account (
     username text NOT NULL,
     password text NOT NULL,
     status text DEFAULT 'OFFLINE'::text NOT NULL,
-    last_activity timestamp with time zone DEFAULT '2019-06-16 19:39:21.307939'::timestamp without time zone NOT NULL,
+    last_activity timestamp with time zone DEFAULT now(),
     credit integer DEFAULT 30,
     email text,
     created_at timestamp with time zone DEFAULT now(),
     updated_at timestamp with time zone DEFAULT now(),
     status_duration integer,
-    notes text
+    notes text,
+    campaign_id integer
 );
 CREATE FUNCTION public.available_credit(account_row public.account) RETURNS bigint
     LANGUAGE sql STABLE
@@ -26,13 +27,13 @@ $$;
 CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
-DECLARE
+declare
   _new record;
-BEGIN
-  _new := NEW;
-  _new."updated_at" = NOW();
-  RETURN _new;
-END;
+begin
+  _new := new;
+  _new. "updated_at" = now();
+  return _new;
+end;
 $$;
 CREATE SEQUENCE public.accounts_id_seq
     AS integer
@@ -54,7 +55,11 @@ CREATE TABLE public.campaign (
     created_at timestamp with time zone DEFAULT now(),
     updated_at timestamp with time zone DEFAULT now(),
     cost_per_one integer DEFAULT 6 NOT NULL,
-    overwrite text DEFAULT 'no'::text NOT NULL
+    overwrite text DEFAULT 'no'::text NOT NULL,
+    repeat integer DEFAULT 0 NOT NULL,
+    repeated integer DEFAULT 0 NOT NULL,
+    limited boolean DEFAULT true NOT NULL,
+    accounts_number integer
 );
 CREATE SEQUENCE public.campaign_id_seq
     AS integer
@@ -80,14 +85,14 @@ CREATE TABLE public.like_campaign (
     type text NOT NULL,
     cost_per_one integer DEFAULT 6 NOT NULL
 );
-CREATE SEQUENCE public."likeCampaign_id_seq"
+CREATE SEQUENCE public.like_campaign_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-ALTER SEQUENCE public."likeCampaign_id_seq" OWNED BY public.like_campaign.id;
+ALTER SEQUENCE public.like_campaign_id_seq OWNED BY public.like_campaign.id;
 CREATE TABLE public.log (
     id integer NOT NULL,
     account_id integer,
@@ -108,9 +113,9 @@ ALTER SEQUENCE public.logs_id_seq OWNED BY public.log.id;
 CREATE TABLE public.setting (
     id integer NOT NULL,
     name text NOT NULL,
-    value jsonb NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    value text
 );
 CREATE SEQUENCE public.setting_id_seq
     AS integer
@@ -140,7 +145,7 @@ CREATE SEQUENCE public.user_id_seq
 ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
 ALTER TABLE ONLY public.account ALTER COLUMN id SET DEFAULT nextval('public.accounts_id_seq'::regclass);
 ALTER TABLE ONLY public.campaign ALTER COLUMN id SET DEFAULT nextval('public.campaign_id_seq'::regclass);
-ALTER TABLE ONLY public.like_campaign ALTER COLUMN id SET DEFAULT nextval('public."likeCampaign_id_seq"'::regclass);
+ALTER TABLE ONLY public.like_campaign ALTER COLUMN id SET DEFAULT nextval('public.like_campaign_id_seq'::regclass);
 ALTER TABLE ONLY public.log ALTER COLUMN id SET DEFAULT nextval('public.logs_id_seq'::regclass);
 ALTER TABLE ONLY public.setting ALTER COLUMN id SET DEFAULT nextval('public.setting_id_seq'::regclass);
 ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
@@ -153,7 +158,7 @@ ALTER TABLE ONLY public.campaign
 ALTER TABLE ONLY public.campaign_type
     ADD CONSTRAINT campaign_type_pkey PRIMARY KEY (name);
 ALTER TABLE ONLY public.like_campaign
-    ADD CONSTRAINT "likeCampaign_pkey" PRIMARY KEY (id);
+    ADD CONSTRAINT like_campaign_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.log
     ADD CONSTRAINT logs_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.setting
@@ -166,8 +171,8 @@ CREATE TRIGGER set_public_account_updated_at BEFORE UPDATE ON public.account FOR
 COMMENT ON TRIGGER set_public_account_updated_at ON public.account IS 'trigger to set value of column "updated_at" to current timestamp on row update';
 CREATE TRIGGER set_public_campaign_updated_at BEFORE UPDATE ON public.campaign FOR EACH ROW EXECUTE PROCEDURE public.set_current_timestamp_updated_at();
 COMMENT ON TRIGGER set_public_campaign_updated_at ON public.campaign IS 'trigger to set value of column "updated_at" to current timestamp on row update';
-CREATE TRIGGER "set_public_likeCampaign_updated_at" BEFORE UPDATE ON public.like_campaign FOR EACH ROW EXECUTE PROCEDURE public.set_current_timestamp_updated_at();
-COMMENT ON TRIGGER "set_public_likeCampaign_updated_at" ON public.like_campaign IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE TRIGGER set_public_like_campaign_updated_at BEFORE UPDATE ON public.like_campaign FOR EACH ROW EXECUTE PROCEDURE public.set_current_timestamp_updated_at();
+COMMENT ON TRIGGER set_public_like_campaign_updated_at ON public.like_campaign IS 'trigger to set value of column "updated_at" to current timestamp on row update';
 CREATE TRIGGER set_public_setting_updated_at BEFORE UPDATE ON public.setting FOR EACH ROW EXECUTE PROCEDURE public.set_current_timestamp_updated_at();
 COMMENT ON TRIGGER set_public_setting_updated_at ON public.setting IS 'trigger to set value of column "updated_at" to current timestamp on row update';
 CREATE TRIGGER set_public_user_updated_at BEFORE UPDATE ON public."user" FOR EACH ROW EXECUTE PROCEDURE public.set_current_timestamp_updated_at();
@@ -177,10 +182,10 @@ ALTER TABLE ONLY public.campaign
 ALTER TABLE ONLY public.campaign
     ADD CONSTRAINT campaign_type_fkey FOREIGN KEY (type) REFERENCES public.campaign_type(name) ON UPDATE SET DEFAULT ON DELETE SET DEFAULT;
 ALTER TABLE ONLY public.like_campaign
-    ADD CONSTRAINT "likeCampaign_account_id_fkey" FOREIGN KEY (account_id) REFERENCES public.account(id) ON UPDATE RESTRICT ON DELETE CASCADE;
-ALTER TABLE ONLY public.like_campaign
-    ADD CONSTRAINT "likeCampaign_user_compaign_id_fkey" FOREIGN KEY (user_compaign_id) REFERENCES public.campaign(id) ON UPDATE RESTRICT ON DELETE CASCADE;
+    ADD CONSTRAINT like_campaign_account_id_fkey FOREIGN KEY (account_id) REFERENCES public.account(id) ON UPDATE RESTRICT ON DELETE CASCADE;
 ALTER TABLE ONLY public.like_campaign
     ADD CONSTRAINT like_campaign_type_fkey FOREIGN KEY (type) REFERENCES public.campaign_type(name) ON UPDATE SET DEFAULT ON DELETE SET DEFAULT;
+ALTER TABLE ONLY public.like_campaign
+    ADD CONSTRAINT like_campaign_user_compaign_id_fkey FOREIGN KEY (user_compaign_id) REFERENCES public.campaign(id) ON UPDATE RESTRICT ON DELETE CASCADE;
 ALTER TABLE ONLY public.log
     ADD CONSTRAINT logs_account_id_fkey FOREIGN KEY (account_id) REFERENCES public.account(id) ON UPDATE RESTRICT ON DELETE CASCADE;
